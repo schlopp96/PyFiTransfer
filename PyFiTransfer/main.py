d884f78e409836ec4707db42f858393c9b915486
schlopp96 <71921821+schlopp96@users.noreply.github.com>
2022-03-13 04:53:18 -0500

    - Large update to logging and file structure.
    Signed-off-by: schlopp96 <71921821+schlopp96@users.noreply.github.com>

--- PyFiTransfer/main.py
+++ PyFiTransfer/main.py
@@ -2,29 +2,55 @@
 #< PyFiTransfer >#
 #& Est. 1/15/22 $#
 
-from os import PathLike
+import logging
+from os import PathLike, chdir
 from os import scandir as lsContents
+from os.path import dirname, exists as isDir
 from os.path import basename as base
-from os.path import exists as isDir
 from shutil import move as copyfile
 from typing import Any, NoReturn
 
-from PyLoad import load
+from PyLoadBar import load
+
+#> Set CWD:
+chdir(dirname(__file__))
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('[%(asctime)s : %(levelname)s] - %(message)s')
+
+logHandler = logging.FileHandler('./logs/log.log', 'a')
+
+logHandler.setFormatter(formatter)
+
+logger.addHandler(logHandler)
+
+textborder: str = '\n========================\n'
 
 
 def main() -> NoReturn | None:
-    """Program entry point.
+    """Program entry point/wrapper.
 
     :return: Program starting method.
     :rtype: NoReturn | None
     """
+    logger.debug('> Started PyFiTransfer... <\n')
+
     origin: str | PathLike = getOrigin()
+    logger.info(f'Got origin directory:\n====> "{origin}"')
+
     destination: str | PathLike = getDestination()
+    logger.info(f'Got file destination:\n====> "{destination}"')
+
     fileType: str = getFileType()
+    logger.info(f'Got file-type to be transferred:\n====> ".{fileType}"')
+
     originClearance: bool = verify(origin)
     destinationClearance: bool = verify(destination)
 
     if originClearance and destinationClearance:
+        logger.info('\n\t\t====> Transferring files now... <====')
         transfer(origin, destination, fileType)
         return Ex_0()
     return Ex_1(fileType)
@@ -65,18 +91,26 @@ def getFileType() -> str:
 
 def verify(directory: PathLike | str) -> bool:
     try:
+        logger.info(
+            f'Verifying directory of given file location:\n====> "{directory}"....'
+        )
         if isDir(directory):
             load(
-                f'\nVerifying destination for file location:\n> "{directory}"',
+                f'\nVerifying destination for file location:\n====> "{directory}"',
                 '\nDirectory verified successfully!', False)
+            logger.info('Directory verified successfully.')
             return True
         else:
+            logger.warning(
+                f'- ERROR -\n> Directory: "{directory}"\n====> Unable to be found, thus could NOT be verified.'
+            )
             load(
-                f'\nVerifying destination for file location:\n> "{directory}"',
-                f'> ERROR\n> Directory: "{directory}"\n> Unable to be found, thus could NOT be verified.',
+                f'\nVerifying destination for file location:\n====> "{directory}"',
+                f'> ERROR\n> Directory: "{directory}"\n====> Unable to be found, thus could NOT be verified.',
                 False)
             return False
-    except (OSError, ValueError, TypeError) as error:
+    except (OSError, ValueError, TypeError, EOFError) as error:
+        logger.exception(f'> ERROR!\n> {error}')
         print(f'> ERROR!\n> {error}')
         return False
 
@@ -88,12 +122,16 @@ def transfer(origin, destination, fileType) -> Any:
         for file in dirFiles:
             if not file.name.startswith('.') and file.name.endswith(
                     f'.{fileType}') == True and file.is_file():
+                logger.info(f'====> Transferring file {file.name}...')
                 files.append(file.name)
-                copyfile(file, (destination + "\\" + base(file)))
+                copyfile(file, f'{destination}\{base(file)}')
+        logger.info(
+            f'{len(files)} files successfully copied to new location:\n{files}'
+        )
         load(
-            f'> Transferring all ".{fileType}" type files to {destination}',
+            f'> Transferring all ".{fileType}" files to: "{destination}"',
             f'> {len(files)} files successfully copied to new location:\n{files}',
-            time=30)
+            time=len(files) if files else 5)
 
 
 def Ex_0() -> NoReturn | None:
@@ -102,7 +140,10 @@ def Ex_0() -> NoReturn | None:
     :return: Exits program.
     :rtype: NoReturn
     """
-    input('\n\nOperation Successfull!\n\nPress ENTER to exit.')
+    logger.debug(
+        f'\n====> Operation Successful! <====\n====>Exiting Program...{textborder}'
+    )
+    print('\n\nOperation Successfull!\n\nExiting Program...')
     return exit()
 
 
@@ -114,8 +155,11 @@ def Ex_1(fileType: str) -> NoReturn | None:
     :return: Exits program.
     :rtype: NoReturn
     """
-    input(
-        f'\n\n> Operation Failure!\n\n> No Files were found with given extension: ".{fileType}".\n\nPress ENTER to exit.'
+    logger.warning(
+        f'\n====> Operation Failure! <====\n====> No Files were found with given extension: ".{fileType}".\n====>Exiting Program...{textborder}'
+    )
+    print(
+        f'\n\n> Operation Failure!\n\n> No Files were found with given extension: ".{fileType}".\n\nExiting Program...'
     )
     return exit()
 
